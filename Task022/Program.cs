660ba9f65e8677300081c6a0a9e00db9282f61b7
kolwaii <kolwaii.kz0202@mail.ru>
2023-04-20 18:39:27 +0600

    Выполил Task022

---
+++ Task022/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+int Input(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+void Table(int num)
+{
+    int index;
+    index = 1;
+    while (index <= num)
+    {
+        Console.WriteLine($"{index, 3}  {index * index, 4}");
+        index++;
+    }
+}
+
+
+int number = Input ("Введите число: ");
+Table(number);
